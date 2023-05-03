158073bf450fcfba0867570bff833a04ecbcf0cb
dhruvdclanguage <126230133+dhruvdclanguage@users.noreply.github.com>
2023-05-03 12:25:10 +0530

    Add files via upload

---
+++ day 13.1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+class Message{
+	
+	private :
+		
+		char f_n[30];
+		
+	public :
+	   
+	    Message(char v[]){
+	    	
+	    	strcpy(f_n,v);
+		}
+		
+		void print(){
+			
+			cout<<"Hello Dhairya"<<endl;
+		}
+		
+		void print(char l_n[]){
+			
+			cout<<f_n<<" "<<l_n<<endl;
+		}	
+};
+
+int main(){
+	
+	Message obj("My name is");
+	
+	obj.print();
+	obj.print("Dhruv\ni am your bench partner in RNW 3");
+	
+	return 0;
+}
